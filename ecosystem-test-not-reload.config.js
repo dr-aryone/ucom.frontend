639ad3f0e8247cf34ed8c188e2427502037d78b5
andreyelek <sukanov2015@yandex.ru>
2019-04-01 14:24:46 +0300

    fix #140

--- ecosystem-test-not-reload.config.js
+++ ecosystem-test-not-reload.config.js
@@ -8,7 +8,6 @@ module.exports = {
       name: `${NODE_ENV}_frontend_renderer`,
       instance_var: 'INSTANCE_ID',
       script: 'server.js',
-      watch: false,
       env: {
         PORT: HTTP_SERVER_PORT,
         autorestart: true,
