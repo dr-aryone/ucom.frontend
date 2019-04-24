45836da973ff6d2ab50283c3df9d06d03e84a681
dimalitvinov <dima8litvinov@gmail.com>
2019-04-24 15:51:42 +0300

    #168 fix socialnetworks

--- src/pages/Pro.jsx
+++ src/pages/Pro.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Redirect } from 'react-router';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Element } from 'react-scroll';
 import urls from '../utils/urls';
 import { getUserById } from '../store/users';
@@ -12,13 +12,13 @@ import Popup from '../components/Popup/';
 import Content from '../components/Popup/Content';
 import VerticalMenu from '../components/VerticalMenu';
 import styles from './Profile.css';
-import DropZone from '../components/DropZone';
 import TextInput from '../components/TextInput';
 import Textarea from '../components/Textarea';
 import Button from '../components/Button/index.jsx';
 import Avatar from '../components/EntryHeader/Avatar';
 // import SocialNetworks from '../components/SN';
 import { validateFields } from '../utils/validateFields';
+import IconRemove from '../components/Icons/Remove';
 
 const Profile = (props) => {
   // Object.keys(props.user).length
@@ -29,18 +29,27 @@ const Profile = (props) => {
   }
 
   const [userData, setUserData] = useState(owner);
+  // const [usersSources, setUsersSources] = useState(userData.setUsersSources);
   const [errors] = useState({});
   const [isSubmited, setIsSubmited] = useState(false);
-  const [sites, setSites] = useState([]);
-  // const [socialNetworks, SocialNetworks] = useState([]);
+  const [socialNetworks, setSocialNetworks] = useState(userData.usersSources || []);
 
   // useEffect(() => [], [user]);
   // const validateUserData = () => {
   //   validateFields();
   // };
+  const addSocialInput = (index, value) => {
+    // sourceUrl => setSocialNetworks(Object.assign([], socialNetworks, { [index]: { ...socialNetworks[index], sourceUrl } }))
+
+    // setUserData({ ...userData, usersSources: Object.assign([], userData.usersSources, { [index]: { 'sourceUrl': value } }) });
+    // console.log('data: ', userData);
+
+    // setUserData({ ...userData, socialNetworks: Object.assign([], userData.usersSources, { [index]: { ...socialNetworks[index], value } }) });
+    // console.log('data: ', userData);
+
+  };
 
   useEffect(() => {
-    console.log('userData: ', userData);
     setUserData({
       ...userData,
       nickname: owner.nickname,
@@ -51,10 +60,13 @@ const Profile = (props) => {
     });
   }, (owner));
 
-  console.log(userData || 'false');
+  // useEffect(() => {
+  //   setUserData([...userData.usersSources, socialNetworks]);
+  // }, (socialNetworks));
 
-  console.log('sites: ', sites.map((value, index) => console.log(index, value.sourceUrl)));
-  console.log('sss: ', sites);
+  // console.log('sites: ', socialNetworks.map((value, index) => console.log(index, value.sourceUrl)));
+  console.log('sss: ', socialNetworks);
+  console.log(userData.usersSources);
 
   return (
     <div className={styles.page}>
@@ -63,12 +75,18 @@ const Profile = (props) => {
           <div className={styles.profile}>
             <div className={styles.sidebar}>
               <VerticalMenu
+                sticky
                 sections={[
                   { name: 'PersonalInfo', title: 'Personal info' },
                   { name: 'AboutMe', title: 'About Me' },
                   { name: 'Links', title: 'Links' },
                 ]}
               />
+              {owner ? (
+                <div className={styles.btnSave}><Button>Save Changes</Button></div>
+              ) : (
+                <div className={styles.btnSave}><Button red>Save Changes</Button></div>
+              )}
             </div>
             <div className={styles.content}>
               <div className={styles.header}>
@@ -94,10 +112,6 @@ const Profile = (props) => {
                           }}
                         />
                       </div>
-                      {/* <DropZone
-                        onDrop={files => this.props.userFormSetForm({ avatarFilename: files[0] })}
-                        text="Add or drag img"
-                      /> */}
                       <div>
                         Drag and drop. We support JPG, PNG or GIF files. Max file size 0,5 Mb.
                       </div>
@@ -130,32 +144,44 @@ const Profile = (props) => {
                 <Element name="Links" className={styles.section}>
                   <h3 className={styles.title}>Links</h3>
                   <div className={styles.field}>
-                    <div className={styles.label}>Website</div>
+                    <div className={styles.label}>My Website</div>
                     <div className={styles.inputBlock}>
-                      {sites.length !== 0 && sites.map((value, index) => (
-                        <TextInput
-                          touched
-                          key={index}
-                          value={value.sourceUrl}
-                          onChange={sourceUrl => setSites(Object.assign([], sites, { [index]: { ...sites[index], sourceUrl } }))}
-                          // className={styles.input}
-                        />
+                      <TextInput
+                      // className={styles.input}
+                        value={userData.personalWebsiteUrl}
+                        onChange={value => setUserData({ ...userData, personalWebsiteUrl: value })}
+                      />
+                    </div>
+                  </div>
+                  <div className={classNames(styles.field, styles.fieldSoical)}>
+                    <div className={styles.label}>Social Networks</div>
+                    <div className={styles.inputBlock}>
+                      {socialNetworks.length !== 0 && socialNetworks.map((value, index) => (
+                        <div className={styles.inputBlockSocial} key={index}>
+                          <TextInput
+                            touched
+                            value={value.sourceUrl}
+                            onChange={sourceUrl => setSocialNetworks(Object.assign([], socialNetworks, { [index]: { ...socialNetworks[index], sourceUrl } }))}
+                            // onChange={sourceUrl => setUserData({ ...userData, usersSources: Object.assign([], userData.usersSources, { [index]: { ...userData.usersSources[index], sourceUrl } }) })}
+                            className={styles.input}
+                          />
+                          <div
+                            role="presentation"
+                            className={styles.trashIcon}
+                            // onClick={() => this.removeField(index)}
+                            onClick={() => setSocialNetworks([...socialNetworks.filter((_, i) => index !== i)])}
+                          >
+                            <IconRemove />
+                          </div>
+                        </div>
                       ))}
                       <Button
                         small
                         grayBorder
-                        onClick={() => setSites([...sites, { sourceUrl: '' }])}
+                        onClick={() => setSocialNetworks([...socialNetworks, { sourceUrl: '' }])}
                       >
-                        Add site
+                        Add Another
                       </Button>
-                    </div>
-                  </div>
-                  <div className={styles.field}>
-                    <div className={styles.label}>Social Networks</div>
-                    <div className={styles.inputBlock}>
-                      <TextInput
-                      // className={styles.input}
-                      />
                     </div>
                   </div>
                 </Element>
@@ -171,6 +197,7 @@ const Profile = (props) => {
 Profile.propTypes = {
   users: PropTypes.objectOf(PropTypes.any).isRequired,
   user: PropTypes.objectOf(PropTypes.any).isRequired,
+  updateUser: PropTypes.func.isRequired,
 };
 
 export default connect(
--- src/utils/validateFields.js
+++ src/utils/validateFields.js
@@ -16,3 +16,73 @@ export const validateFields = (
     fields.reduce((value, field) => ({ ...value, [field]: validation.errors.get(field) }), {});
   return { isValid, errors };
 };
+
+
+// let data = {
+//   nickname: 'Alex',
+//   sites: ['http://ya.ru', 'http://test.com'],
+//   partners: [{
+//     url: 'http://ya.ru',
+//     title: 'Home',
+//   }],
+// };
+
+// let rules = {
+//   nickname: value =>
+//     (!value && 'Nickname is reqired') ||
+//     (!NIKNAME_REGEXP.test(value) && 'Nickname is not valid'),
+
+//   sites: [
+//     value =>
+//       (!value && 'Site is reqired') ||
+//       (!URL_REGEXP.test(value) && 'Site is not valid')
+//   ],
+
+//   partners: [{
+//     url: value =>
+//       (!value && 'Site is reqired') ||
+//       (!URL_REGEXP.test(value) && 'Site is not valid'),
+//     title: value =>
+//       (!value && 'Site is reqired') ||
+//       (!URL_REGEXP.test(value) && 'Site is not valid'),
+//   }],
+// };
+
+// let errors = {
+//   nickname: 'Some error',
+//   sites: ['Error', '1 Error'],
+//   partners: [{
+//     url: 'Url error',
+//     title: 'Title error',
+//   }],
+// };
+
+// export const validator = (data, rules) => {
+//   const fields = Object.keys(data);
+//   const errors = {};
+
+//   fields.forEach((field) => {
+//     const rule = rules[field];
+
+//     if (!rule) {
+//       return;
+//     }
+
+//     if (typeof rule === 'function') {
+//       errors[field] = rule(data[field]);
+//     } else if (Array.isArray(rule) && typeof rule[0] === 'function') {
+//       errors[field] = data[field].map(value => rule[0](value));
+//     } else if (Array.isArray(rule) && typeof rule[0] === 'object') {
+//       errors[field] = data[field].map((obj) => {
+//         const keys = Object.keys(rule[0]);
+//         const errors = {};
+//         keys.forEach((key) => {
+//           errors[key] = rule[0][key](obj[key]);
+//         });
+//         return errors;
+//       });
+//     }
+//   });
+
+//   return errors;
+// };
